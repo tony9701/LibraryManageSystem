3f2e465f5360f9d5866d3ab29f5cfccbf8ef1a4d
tony9701 <80263851+tony9701@users.noreply.github.com>
2023-12-16 20:39:17 +0200

    Update ExceptionMessages.java
    Added some more messages.

--- src/main/java/Common/ExceptionMessages/ExceptionMessages.java
+++ src/main/java/Common/ExceptionMessages/ExceptionMessages.java
@@ -3,5 +3,8 @@ package Common.ExceptionMessages;
 public class ExceptionMessages {
 
     public static final String BOOK_NAME_EMPTY_OR_NULL = "Invalid book name!";
+    public static final String GENRE_EMPTY_OR_NULL = "Invalid genre!";
+    public static final String AUTHOR_NAME_EMPTY_OR_NULL = "Invalid author!";
+    public static final String INVALID_PUBLISHED_YEAR = "Invalid year!";
 
 }
