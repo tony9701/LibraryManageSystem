71b0c694758b3146ba264d49bb7eea9217db9c56
tony9701 <80263851+tony9701@users.noreply.github.com>
2023-12-16 20:06:55 +0200

    Created ExceptionMessages Class!
    That class will handle all the Exception Messeges in the app.

---
+++ src/main/java/Common/ExceptionMessages/ExceptionMessages.java
@@ -0,0 +1,7 @@
+package Common.ExceptionMessages;
+
+public class ExceptionMessages {
+
+    public static final String BOOK_NAME_EMPTY_OR_NULL = "Invalid book name!";
+
+}
--- src/main/java/Models/Book/Book.java
+++ src/main/java/Models/Book/Book.java
@@ -1,14 +1,61 @@
 package Models.Book;
 
+import static Common.ExceptionMessages.ExceptionMessages.*;
+
 public class Book {
 
-    private String title;
-    private String genre;
-    private String author;
-    private String publishedYear;
-    private boolean availabilityStatus;
+    private final String title;
+    private final String genre;
+    private final String author;
+    private final String publishedYear;
+    private boolean isAvailable;
 
-    
+    public Book(String title, String genre, String author, String publishedYear) {
+        setTitle(title);
+        this.genre = genre;
+        this.author = author;
+        this.publishedYear = publishedYear;
+        this.isAvailable = true;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    private void setTitle(String title) {
+        if (title == null || title.isBlank()) {
+            throw new IllegalArgumentException(BOOK_NAME_EMPTY_OR_NULL);
+        }
+
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPublishedYear() {
+        return publishedYear;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    private void setAvailable(boolean available) {
+        this.isAvailable = available;
+    }
 
 
+    public void checkIn() {
+        setAvailable(true);
+    }
+
+    public void checkOut() {
+        setAvailable(false);
+    }
 }
