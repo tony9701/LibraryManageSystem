4bd435360d9ae5ef384f33a5b7fa4d5b81cd6333
tony9701 <80263851+tony9701@users.noreply.github.com>
2023-12-17 12:20:36 +0200

    Update Validator.java
    Added some simple email validations with regex pattern

--- src/main/java/Utils/Validator.java
+++ src/main/java/Utils/Validator.java
@@ -1,11 +1,17 @@
 package Utils;
 
 import java.time.Year;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Validator {
 
     private static final int CURRENT_YEAR = Year.now().getValue();
     private static final int FIRST_BOOK_YEAR = 1440;
+    private static final String EMAIL_PATTERN =
+            "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+    private static final Pattern pattern = Pattern.compile(EMAIL_PATTERN);
 
     public static boolean nameIsValid(String name) {
         return name != null && !name.isBlank();
@@ -13,5 +19,10 @@ public class Validator {
 
     public static boolean publishedYearIsValid(int publishedYear) {
         return publishedYear >= FIRST_BOOK_YEAR && publishedYear <= CURRENT_YEAR;
+    }
+
+    public static boolean emailIsValid(String email) {
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 }
