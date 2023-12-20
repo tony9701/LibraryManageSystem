51dd991ca5dd6af3b896dee9ef9c640c12dc480d
tony9701 <80263851+tony9701@users.noreply.github.com>
2023-12-20 23:06:22 +0200

    Create ConstantMessages.java

---
+++ src/main/java/Common/ConstantMessages/ConstantMessages.java
@@ -0,0 +1,8 @@
+package Common.ConstantMessages;
+
+public class ConstantMessages {
+    public static final String BOOK_ADDED = "%s added successfully in the library!";
+    public static final String BOOK_REMOVED = "%s removed successfully from the library!";
+    public static final String MEMBER_ADDED = "%s added successfully as a new member!";
+    public static final String MEMBER_REMOVED = "%s successfully end her/his membership!";
+}
