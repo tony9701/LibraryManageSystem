2445000a4e9a6d36dac7f24b991de925d9145cdb
tony9701 <80263851+tony9701@users.noreply.github.com>
2023-12-23 14:12:10 +0200

    added the engine in the main method so it can run the app

--- src/main/java/LibraryManagementApp.java
+++ src/main/java/LibraryManagementApp.java
@@ -1,5 +1,8 @@
+import Core.Engine.EngineImpl;
+
 public class LibraryManagementApp {
     public static void main(String[] args) {
-
+        EngineImpl engine = new EngineImpl();
+        engine.run();
     }
 }
