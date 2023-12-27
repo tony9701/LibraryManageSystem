df851fb1805a8cf1c69e31a1b46617b41277c4b2
tony9701 <80263851+tony9701@users.noreply.github.com>
2023-12-27 23:04:56 +0200

    Update Library.java

--- src/main/java/Models/Library/Library.java
+++ src/main/java/Models/Library/Library.java
@@ -21,6 +21,6 @@ public interface Library {
     void addMember(LibraryMember member);
     void removeMember(LibraryMember member);
     LibraryMember searchMember(String name);
-    boolean bookIsAvailable(String title);
+    Book bookIsAvailable(String title);
 
 }
