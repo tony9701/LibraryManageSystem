c29b283bc8fb950f6833f039bc7ee711e1c7ca03
tony9701 <80263851+tony9701@users.noreply.github.com>
2023-12-31 12:40:51 +0200

    Update ConstantMessages.java

--- src/main/java/Common/ConstantMessages/ConstantMessages.java
+++ src/main/java/Common/ConstantMessages/ConstantMessages.java
@@ -7,5 +7,6 @@ public class ConstantMessages {
     public static final String MEMBER_REMOVED = "%s successfully end her/his membership!";
     public static final String BORROW_BOOK = "%s successfully borrowed %s.";
     public static final String RETURN_BOOK = "%s successfully returned %s.";
+    public static final String BOOK_RESERVED = "%s successfully reserved %s.";
 
 }
